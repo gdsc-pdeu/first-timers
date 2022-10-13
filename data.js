18346410812c68de732d3846ef8678ac1304a28e
Preet-Sojitra <preet.dev373@gmail.com>
2022-10-13 21:24:09 +0530

    test commit

--- data.js
+++ data.js
@@ -15,6 +15,21 @@ const data = [
   // ======================== <Your name> Profile ENDS ==========================
 
   // Copy the above template and paste it below this line
+
+  // ======================== <Your name> Profile STARTS ========================
+  {
+    id: 0,
+    name: "add your name",
+    branch: "add your branch",
+    introduction: "add your small introduction",
+    skills: ["add your skills", "skill1", "skill2"],
+    socialsHandles: {
+      github: "add your github handle",
+      twitter: "add your twitter handle",
+      linkedin: "add your linkedin handle",
+    },
+  },
+  // ======================== <Your name> Profile ENDS ==========================
 ]
 
 // To be used later
