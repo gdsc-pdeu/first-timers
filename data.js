a772518c39100cf78a83ae603fafeb03d0bea99e
SShivanjali01~ <shivanjalisriv@gmail.com>
2022-10-14 17:58:04 +0100

    Profile created

--- data.js
+++ data.js
@@ -13,9 +13,18 @@ const data = [
     },
   },
   // ======================== <Your name> Profile ENDS ==========================
-
-  // Copy the above template and paste it below this line
+  {
+    id: 0,
+    name: "add your name",
+    branch: "add your branch",
+    introduction: "add your small introduction",
+    skills: ["add your skills", "skill1", "skill2"],
+    socialsHandles: {
+      github: "add your github handle",
+      twitter: "add your twitter handle",
+      linkedin: "add your linkedin handle",
+    },
+  },
 ]
-
 // To be used later
 export default data
