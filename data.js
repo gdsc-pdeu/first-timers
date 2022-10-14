5cf5de580403a5a2c06d2817a645d8854291a39c
kathan vyas <kathan61004@gmail.com>
2022-10-14 18:00:56 +0530

    add kathan profile

--- data.js
+++ data.js
@@ -2,6 +2,29 @@ const data = [
   // ======================== <Your name> Profile STARTS ========================
   {
     id: 0,
+    name: "kathan vyas",
+    branch: "computer engineering",
+    introduction: "first timer",
+    skills: ["add your skills", "skill1", "skill2"],
+    socialsHandles: {
+      github: "add your github handle",
+      twitter: "add your twitter handle",
+      linkedin: "add your linkedin handle",
+    },
+  },
+  // ======================== <Your name> Profile ENDS ==========================
+
+  // Copy the above template and paste it below this line
+]
+
+// To be used later
+export default data
+
+
+const data = [
+  // ======================== <Your name> Profile STARTS ========================
+  {
+    id: 0,
     name: "add your name",
     branch: "add your branch",
     introduction: "add your small introduction",
@@ -18,4 +41,5 @@ const data = [
 ]
 
 // To be used later
-export default data
+export default data 
+
