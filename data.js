a32440031b7d0ea289c6cabc9b05c8fb61197a16
Jay Nakum <nakum.jay.m@gmail.com>
2022-10-14 17:45:13 +0530

    add jay profile

--- data.js
+++ data.js
@@ -15,6 +15,21 @@ const data = [
   // ======================== <Your name> Profile ENDS ==========================
 
   // Copy the above template and paste it below this line
+
+  // ======================== <Your name> Profile STARTS ========================
+  {
+    id: 1,
+    name: "Jay Nakum",
+    branch: "CSE",
+    introduction: "I am Jay Nakaum.",
+    skills: ["JavaScript", "Flutter"],
+    socialsHandles: {
+      github: "JayNakum",
+      twitter: "add your twitter handle",
+      linkedin: "add your linkedin handle",
+    },
+  },
+  // ======================== <Your name> Profile ENDS ==========================
 ]
 
 // To be used later
