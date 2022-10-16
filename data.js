2fb3f1bf779ac44734fca687c7123eae71156fff
Raj Randive <randiveraj96@gmail.com>
2022-10-16 10:53:59 +0530

    Raj's Profile Added

--- data.js
+++ data.js
@@ -15,18 +15,19 @@ const data = [
   // ======================== <Your name> Profile ENDS ==========================
 
   // Copy the above template and paste it below this line
-      {
-      id: 1,
-      name: "Om shah",
-      branch: "Computer Science",
-      introduction: "Student at PDEU",
-      skills: ["add your skills", "skill1", "skill2"],
-      socialsHandles: {
-        github: "add your github handle",
-        twitter: "add your twitter handle",
-        linkedin: "add your linkedin handle",
-      },
-      },
+  {
+    id: 1,
+    name: "Om shah",
+    branch: "Computer Science",
+    introduction: "Student at PDEU",
+    skills: ["add your skills", "skill1", "skill2"],
+    socialsHandles: {
+      github: "add your github handle",
+      twitter: "add your twitter handle",
+      linkedin: "add your linkedin handle",
+    },
+  },
+
   {
     id: 0,
     name: "Dinky Unadkat",
@@ -39,6 +40,21 @@ const data = [
       linkedin: "Dinky Unadkat",
     },
   },
+
+  {
+    id: 13,
+    name: "Raj Randive",
+    branch: "Computer Science and Engineering",
+    introduction: "Hey There!, I am Raj Randive, Web-Developer and open-source enthusiast",
+    skills: ["C/C++", "Java", "JavaScript", "Reactjs", "Flutter", "Python"],
+    socialsHandles: {
+      github: "https://github.com/Raj-Randive",
+      twitter: "https://twitter.com/RajRandive_",
+      linkedin: "https://www.linkedin.com/in/rajrandive14/",
+    },
+  },
+
+
 ]
 
 // To be used later
